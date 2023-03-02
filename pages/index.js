0647298b9c34c78400ac7942f3a5ad937049ea1b
Aidan <datbluemushroom@gmail.com>
2023-03-02 11:08:21 -0500

    ➖ TikTok API (also didnt work)

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react'
 
 export default function Home() 
 {
-  // const [data, setData] = useState([{}])
+
+  const [data, setData] = useState([{}])
 
   // useEffect(() => 
   // {
